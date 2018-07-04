2740b20841ea019fe6ed755831f8156ba33fabcc
ishiland <ishiland@gmail.com>
2018-07-04 09:28:57 -0400

    eslint

--- cmv-widgets/js/gis/plugins/AdvancedDraw/widget/_ColorMixin.js
+++ cmv-widgets/js/gis/plugins/AdvancedDraw/widget/_ColorMixin.js
@@ -3,5 +3,5 @@
  *  Project: https://cmv.io/
  */
 
-define(["dojo/_base/declare","dojo/_base/Color"],function(o,r){return o(null,{_esriColorArrayToDojoColor:function(o){return o[3]=o[3]/255,r.fromArray(o)},_dojoColorToEsriColorArray:function(o){var r=o.toRgba();return r[3]=Math.round(255*r[3]),r}})});
+define(["dojo/_base/declare","dojo/_base/Color"],function(o,r){return o(null,{_esriColorArrayToDojoColor:function(o){return o[3]/=255,r.fromArray(o)},_dojoColorToEsriColorArray:function(o){var r=o.toRgba();return r[3]=Math.round(255*r[3]),r}})});
 //# sourceMappingURL=_ColorMixin.js.map
